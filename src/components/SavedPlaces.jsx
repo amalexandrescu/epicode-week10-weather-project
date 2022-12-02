bb864cee7a7edb088a53e4de094ef745dd3aa815
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-12-03 00:12:13 +0200

    css file cleaned

--- src/components/SavedPlaces.jsx
+++ src/components/SavedPlaces.jsx
@@ -59,39 +59,3 @@ const SavedPlaces = () => {
 };
 
 export default SavedPlaces;
-
-// Cart now needs to read/"write" to the store, reading the value
-// of cart.content and being able to dispatch an action for removing
-// an element from the cart!
-
-// const Favorites = () => {
-//   const favorites = useSelector((state) => state.jobs.favorites);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <Row>
-//       <Col sm={12}>
-//         <ul style={{ listStyle: "none" }}>
-//           {favorites.map((job, i) => (
-//             <li key={i} className="my-4">
-//               <span
-//                 className="text-danger mr-2"
-//                 onClick={() => {
-//                   dispatch({
-//                     type: "REMOVE_FROM_FAVORITES",
-//                     payload: i,
-//                   });
-//                 }}
-//               >
-//                 <Icon.TrashFill />
-//               </span>
-//               {job.title} at {job.company_name} Company
-//             </li>
-//           ))}
-//         </ul>
-//       </Col>
-//     </Row>
-//   );
-// };
-
-// export default Favorites
